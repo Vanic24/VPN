10575856db05f2f6d2e89315841c42ac11e42a1c
Vanic <74780731+Vanic24@users.noreply.github.com>
2025-09-12 11:23:48 +0630

    Update Filter.py

--- src/Filter.py
+++ src/Filter.py
@@ -510,6 +510,7 @@ def upload_to_textdb_from_raw():
 if __name__ == "__main__":
     try:
         main()
+        upload_to_textdb(output_text)
     except Exception as e:
         print("[FATAL ERROR]", str(e))
         traceback.print_exc()
