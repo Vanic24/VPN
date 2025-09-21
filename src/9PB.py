0a3a611204750113bfee7d3eb23195cd6e1d4064
Vanic <74780731+Vanic24@users.noreply.github.com>
2025-09-21 11:07:54 +0630

    Update 9PB.py

--- src/9PB.py
+++ src/9PB.py
@@ -14,6 +14,7 @@ import re
 import pycountry
 import json
 import urllib.parse
+from urllib.parse import unquote
 
 # ---------------- Config ----------------
 REPO_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
