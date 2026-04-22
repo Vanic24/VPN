6811929f9a6f1a7ef3055c9e62081644888e8693
Vanic <74780731+Vanic24@users.noreply.github.com>
2026-04-22 13:41:01 +0630

    Update 9PB.py

--- src/9PB.py
+++ src/9PB.py
@@ -19,7 +19,7 @@ from urllib.parse import unquote, urlparse, parse_qs
 # ---------------- Config ----------------
 REPO_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 OUTPUT_FILE = os.path.join(REPO_ROOT, "9PB")
-OUTPUT_FILE_Clash = os.path.join(REPO_ROOT, "9PB_Clash")
+OUTPUT_FILE_CLASH = os.path.join(REPO_ROOT, "9PB_Clash")
 SOURCES_FILE = os.path.join(REPO_ROOT, "SUB_9PB")
 CLASH_TEMPLATE = os.path.join(REPO_ROOT, "ClashTemplate.ini")
 TEXTDB_API = "https://textdb.online/update/?key=9PB_SHFX&value={}"
